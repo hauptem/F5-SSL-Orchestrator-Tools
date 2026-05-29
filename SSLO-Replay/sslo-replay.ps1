9eb0f9181addb623055f9b0b5f56cb514bf3791b
Eric Haupt <233467505+hauptem@users.noreply.github.com>
2026-05-29 17:10:13 -0400

    Update sslo-replay.ps1

--- SSLO-Replay/sslo-replay.ps1
+++ SSLO-Replay/sslo-replay.ps1
@@ -1,7 +1,7 @@
 ﻿# =============================================================================
 # SSLO-Replay — F5 SSL Orchestrator Snapshot and Restore Tool
 # =============================================================================
-# Version: 0.3.15-devel
+# Version: b2.3.15-devel (Beta 2 29 May 2026)
 # Author: Eric Haupt
 # Released under the MIT License.
 # https://github.com/hauptem/F5-SSL-Orchestrator-Tools
